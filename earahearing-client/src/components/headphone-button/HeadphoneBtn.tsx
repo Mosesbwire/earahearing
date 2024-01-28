08188640d32e3d550e5fe421f1f23c0acc4cda3a
Mosesbwire <mosesdev92@gmail.com>
2024-01-28 23:48:27 +0300

    feat: add headphone button component

---
+++ earahearing-client/src/components/headphone-button/HeadphoneBtn.tsx
@@ -0,0 +1,13 @@
+
+import headphone from '../../assets/HEADPHONE BUTTON.svg'
+import './headphoneBtn.css'
+
+export const HeadphoneBtn = () => {
+    return (
+        <div className='headphone-btn'>
+            <img src={headphone} alt="colored headphone" className='headphone-img' />
+            <p className='text text-dark text-bold vol-level'>1</p>
+        </div>
+    )
+}
+
