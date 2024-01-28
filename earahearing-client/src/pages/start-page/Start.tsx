06b658cd8bcdeb9ec311d1f953e083ce84a049bd
Mosesbwire <mosesdev92@gmail.com>
2024-01-29 01:50:01 +0300

    feat: add Start page
    mobile version

---
+++ earahearing-client/src/pages/start-page/Start.tsx
@@ -0,0 +1,31 @@
+import { Logo } from "../../components/logo/Logo"
+import { Button } from "../../components/button/Button"
+import heroImage from '../../assets/images.jpg'
+import './start-page.css'
+const Start = () => {
+    return (
+        <section className="start-page-wrapper">
+            <header className="container">
+                <Logo/>
+            </header>
+            <div className="hero">
+                <div className="hero--image">
+                    <img src={heroImage} alt="man and woman listening on headphones" />
+                </div>
+                <div className="hero--information container">
+                    <h1 className="headline text-centered">Do you think you have hearing loss?</h1>
+                    <h2 className="subheadline text-dark text-centered">Clinically tested and proved to provide accurate hearing test results</h2>
+                    <h2 className="subheadline text-dark text-centered">Take our 4 minute hearing test and see your results</h2>
+                    <div className="start-btn">
+                        <Button className="btn-primary-rounded">
+                            Start
+                        </Button>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Start
