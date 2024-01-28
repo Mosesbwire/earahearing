d828ad326c89d87e63f88451ce5d63364f98da3c
Mosesbwire <mosesdev92@gmail.com>
2024-01-29 01:49:08 +0300

    Feat: add Logo component

---
+++ earahearing-client/src/components/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import blueLogo from '../../assets/eara29-final.png'
+
+const styles = {
+    'width': '25%'
+}
+
+export const Logo = () => {
+    return (
+        <img src={blueLogo} alt="earahearing logo" style={styles}/>
+    )
+}
