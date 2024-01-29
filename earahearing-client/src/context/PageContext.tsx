f9d87cc0dc460947a3d011bd35509a3991853510
Mosesbwire <mosesdev92@gmail.com>
2024-01-29 22:06:50 +0300

    feat: create PageContext Provider
    provide data and function to multistep through the form process
    
    provide currentPage, nextPage, prevPage

---
+++ earahearing-client/src/context/PageContext.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { PageContext } from "../hooks/usePageContext";
+
+type pageContextProviderProps = {
+    children: React.ReactNode
+}
+export default function PageContextProvider({ children }: pageContextProviderProps){
+    const [currentPage, setCurrentPage] = useState<number>(0)
+    function nextPage(){
+        console.log('clicked')
+        setCurrentPage(page => page + 1)
+    }
+    function previousPage(){
+        setCurrentPage(page => page - 1)
+    }
+    return (
+        <PageContext.Provider value={[currentPage, nextPage, previousPage]}>
+            {children}
+        </PageContext.Provider>
+    )
+}
