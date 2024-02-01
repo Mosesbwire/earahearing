3c38dc93c00c01c044140cb680a2ce4a56a5d46a
Mosesbwire <mosesdev92@gmail.com>
2024-02-01 16:37:54 +0300

    feat: add page diplaying form to collect user details at end of hearing test

---
+++ earahearing-client/src/pages/Results/ResultsForm.tsx
@@ -0,0 +1,33 @@
+
+import { Form } from "../../components/form/Form";
+import { Input } from "../../components/input/Input";
+import { Button } from "../../components/button/Button";
+import BannerImg from "../../assets/CONGRATULATIONS.png"
+
+import './results.css'
+
+
+const ResultsForm = () => {
+    return (
+        <div className="results-form-page container">
+            <div className="banner">
+                <img src={BannerImg} alt="congratulations banner" />
+            </div>
+            <div className="results-form-text">
+                <p className="text text-dark">You have completed the hearing test.</p>
+                <p className="text text-dark">Let's create a quick profile so you can keep record of your hearing test results</p>
+            </div>
+            <Form>
+                <div>
+                    <Input type="text"  name="firstName" label="First Name"/>
+                    <Input type="text" name="lastName" label="Last Name"/>
+                </div>
+                <Input type="tel" name="phoneNumber" label="Phone Number"/>
+                <Input  type="email" name="email" label="Email"/>
+                <Button className="btn-primary-rounded">Get Results</Button>
+            </Form>
+        </div>
+    )
+}
+
+export default ResultsForm
