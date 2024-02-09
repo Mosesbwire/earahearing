3bd3cdfe8cddd99259ace4c8169452c3cb2b0b26
Mosesbwire <mosesdev92@gmail.com>
2024-02-09 16:28:13 +0300

    fix: Add functionality to indicate choice user has selected - show start arrow at every frequency - next button is active if user moves to previous page

--- earahearing-client/src/pages/hearing-tests/Test.tsx
+++ earahearing-client/src/pages/hearing-tests/Test.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button } from "../../components/button/Button"
 import { HeadphoneBtn } from "../../components/headphone-button/HeadphoneBtn"
 import VolumePanel from "../../components/volume/Volume"
 import { ArrowSvg } from "../../components/arrow/ArrowSvg"
 import { usePageContextNext } from "../../hooks/usePageContext"
 import { useProgressContextIncrease, useProgressContextDecrease } from "../../hooks/useProgressContext"
+import { useLocalStorage } from "../../hooks/useLocalStorage"
 import sound from "../../lib/audio"
 import { frequncyRange } from "../../lib/audio"
 import './hearing-test.css'
 
 type freq = Record<string, frequncyRange>
+
 const frequencies = [500, 1000, 2000, 4000, 8000] as const
 const ranges: freq = {
     0: 'freq_5',
@@ -19,23 +21,65 @@ const ranges: freq = {
     4: 'freq_80'
 }
 
+type selectedFreqType = Record<frequncyRange, number>
+
+type dataType = {
+    Left: selectedFreqType,
+    Right: selectedFreqType
+}
+const selectedFrequencies: dataType = {
+    Left: {
+        freq_5: -1,
+        freq_10: -1,
+        freq_20: -1,
+        freq_40: -1,
+        freq_80: -1
+    },
+    Right: {
+        freq_5: -1,
+        freq_10: -1,
+        freq_20: -1,
+        freq_40: -1,
+        freq_80: -1
+    }
+}
+
 const Test = () => {
     const [ear, setEar] = useState<'Left' | 'Right'>('Right')
     const [frequency, setFrequency] = useState(0)
     const [playingHeadphone, setPlayingHeadphone] = useState(-1)
+    const [isSelected, setIsSelected] = useState(-1)
+    const [data, addData, getData] = useLocalStorage<dataType>()
     const [moveToNextFrequency, setMoveToNextFrequency] = useState(false)
     const [isPlaying, setIsPlaying] = useState(false)
     const volumeSteps = Array(10).fill('vol')
     const next = usePageContextNext()
     const incrProgress = useProgressContextIncrease()
     const decrProgress = useProgressContextDecrease()
-    
+
+    useEffect(()=> {
+        if(data){
+            const currFreq = ranges[frequency]
+            const currEar = selectedFrequencies[ear]
+            const selected = currEar[currFreq]
+            setIsSelected(selected)
+        }
+    }, [data, frequency, ear])
+
+    if (isSelected === -1 && !data){
+        addData('freq', selectedFrequencies)
+    }
 
     const nextClickHandler = () => {
         incrProgress()
         setMoveToNextFrequency(true)
         setIsPlaying(false)
         sound.pause()
+        setPlayingHeadphone(-1)
+        const currFreq = ranges[frequency]
+        const currEar = selectedFrequencies[ear]
+        currEar[currFreq] = playingHeadphone
+        addData('freq', selectedFrequencies)
         if (frequency === frequencies.length - 1) {
             setEar('Left')
             setFrequency(0)
@@ -50,7 +94,7 @@ const Test = () => {
     }
 
     const prevClickHandler = () => {
-        setIsPlaying(false)
+        setIsPlaying(true)
         sound.pause()
         setMoveToNextFrequency(true)
         if (ear === 'Right' && frequency !== 0){
@@ -65,12 +109,13 @@ const Test = () => {
         if (frequency !== 0 || ear !== 'Right'){
             decrProgress()
         }
+        getData('freq')   
     }
 
     const playHeadphone = (index: number) => {
         sound.pause()
         sound.hearingTest(ear, ranges[`${frequency}`], index + 1)
-        
+        setIsSelected(-1)
         setPlayingHeadphone(index)
         setMoveToNextFrequency(false)
         setIsPlaying(true)
@@ -79,14 +124,16 @@ const Test = () => {
     return (
         <div className="test">
             <div>
-                <h1 className="headline text-centered">Hearing Test <span className="text-bold">{ear}</span> Ear</h1>
-                <div className="emphasis-accented"></div>
-                <p className="subheadline text-dark text-bold text-centered">{frequencies[frequency]} Hz</p>
+                <h1 className="headline text-bold text-centered">Hearing Test</h1>
+                <p className="subheadline text-dark text-centered"><span className="curr-frequency">{ear} Ear</span> {frequencies[frequency]} Hz</p>
             </div>
             <div className="test-instruction-test">
                 <div className="instruction-mobile">
-                    <p className="text text-dark">Start listening from the <span className="emphasis text-bold">lowest volume setting</span> that you can just <span className="emphasis text-bold">barely</span> hear the sound in your <span className="empahasis text-bold">right</span> ear.</p>
-                    <p className="text text-dark">If you do not hear any sound, increase volume unitll you just barely hear the sound.</p>
+                    <p className="text text-dark">Start at 0</p>
+                    <p className="text text-dark">Sound is now playing.</p>
+                    <p className="text text-dark">Push the +  sign untill you can barely <span className="emphasis-text-accented text-bold">hear</span> the sound in your <span className="text-bold emphasis-text-main-color">{ear}</span></p>
+                    <p className="text text-dark">If you do not hear any sound, click the + sign untill 10.</p>
+                    <p className="text text-dark">Then click Next.</p>
                 </div>
                 <div className="instruction-desktop">
                     <p className="text text-dark">Select the <span className="emphasis text-bold">lowest number headphone</span> that you can just <span className="emphasis text-bold">barely</span> hear the sound in your <span className="empahasis text-bold">right</span> ear.</p>
@@ -96,15 +143,15 @@ const Test = () => {
             <div className="sound-panel">
                 <VolumePanel play={playHeadphone} nextFreq={moveToNextFrequency}/>
             </div>
-            {playingHeadphone === -1 ? <div className="start-arrow">
+            <div className={`start-arrow ${playingHeadphone !== -1 ? 'toggle-arrow' : ''}`}>
                 <ArrowSvg className="arrow"/>
                 <div className="start-text">
                     <p className="text text-dark text-bold">START</p>
                 </div>
-            </div> : null}
+            </div>
             <div className="sound-panel-desktop">
                 {volumeSteps.map((vol,idx) => (
-                    <HeadphoneBtn key={`${vol+ idx}`} volume={idx + 1} index={idx} active={playingHeadphone} next={moveToNextFrequency} onClick={playHeadphone}/>
+                    <HeadphoneBtn key={`${vol+ idx}`} volume={idx + 1} index={idx} active={playingHeadphone} selected={isSelected} next={moveToNextFrequency} onClick={playHeadphone}/>
                 ))}
             </div>
             <div className="test-direction row">
