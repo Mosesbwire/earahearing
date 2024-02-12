f4e07ea3a12c71eea7dcb6b1c01db706e215c744
Mosesbwire <mosesdev92@gmail.com>
2024-02-12 21:03:34 +0300

    fix: css - modify margin, padding and font-sizes to fit page content on single page with no scrollng

--- earahearing-client/src/pages/Results/ResultsForm.tsx
+++ earahearing-client/src/pages/Results/ResultsForm.tsx
@@ -14,9 +14,7 @@ const ResultsForm = () => {
     }
     return (
         <div className="results-form-page container">
-            <div className="banner">
-                {/* <h1 className="text-dark text-bold congrats">Congratulations</h1> */}
-            </div>
+    
             <div className="results-form-text text-centered">
                 <p className="text text-dark ">You have completed the hearing test.</p>
                 <p className="text text-dark create-txt">Let's create a quick profile so you can keep record of your hearing test results</p>
