903b266ae16d582482779b4c57561a068920c600
Mosesbwire <mosesdev92@gmail.com>
2024-02-16 17:00:57 +0300

    update: Add route for the form page

--- earahearing-client/src/App.tsx
+++ earahearing-client/src/App.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './pages/home/Home'
+import Confirmation from './pages/confirmation/confirmation'
+import ResultsForm from './pages/Results/ResultsForm'
 import { Logo } from './components/logo/Logo'
 import PageContextProvider from './context/PageContext'
 import ProgressContextProvider from './context/ProgressContext'
@@ -17,6 +19,8 @@ function App() {
           <ProgressContextProvider>
             <Routes>
               <Route path='/' element={<Home/>}/>
+              <Route path='/confirm-email' element={<Confirmation/>}/>
+              <Route path='/form' element={<ResultsForm/>}/>
             </Routes>
           </ProgressContextProvider>
         </PageContextProvider>
