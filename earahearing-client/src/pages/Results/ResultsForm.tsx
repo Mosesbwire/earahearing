0bbf635f1a163b49857b52d8f26b57ca33e7f68a
Mosesbwire <mosesdev92@gmail.com>
2024-02-17 02:29:15 +0300

    feat: make form functional - use useForm hook to collect user data. - install yup, validate user data with yup - show validation errors on input

--- earahearing-client/src/pages/Results/ResultsForm.tsx
+++ earahearing-client/src/pages/Results/ResultsForm.tsx
@@ -1,20 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import * as Yup from 'yup'
+import { useForm } from "../../hooks/useForm";
 import { Form } from "../../components/form/Form";
 import { Input } from "../../components/input/Input";
 import { Button } from "../../components/button/Button";
 
 import './results.css'
-
-
-
 export const TOTAL_RESULT_PAGES = 1
+
+type Inputs = Record<string, string>
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 const ResultsForm = () => {
 
-    const submit = (e: React.FormEvent<HTMLFormElement>) => {
+    const validationSchema = Yup.object().shape({
+        firstName: Yup.string().trim().min(1, 'Name must be more than one character long').required('First Name is required'),
+        lastName: Yup.string().trim().min(1, 'Name must be more than one character long ').required('Last Name is required'),
+        email: Yup.string().trim().email('Enter a valid email').required('Email is required'),
+        phoneNumber: Yup.string().trim().matches(phoneRegExp, {message: 'Enter a valid phone number'}).required('Phone number is required')
+    })
+    const initialValues: Inputs = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: ''
+    }
+
+    type validationErrorsType = {
+        firstName?: string,
+        lastName?: string,
+        email?: string,
+        phoneNumber?: string
+    }
+
+    const validationErrors: validationErrorsType = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: ''
+    }
+
+    const [values, handleChange, resetForm] = useForm<Inputs>(initialValues)
+    const [formErrors, setFormErrors] = useState(validationErrors)
+
+    const onInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+        handleChange(e)
+        const key = e.currentTarget.name
+        setFormErrors({...formErrors, [key]: ''})
+    }
+
+    const submit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         
-
+        validationSchema.validate(values, {abortEarly: false}).then(valid => {
+            console.log(valid)
+            resetForm()
+        }).catch((validationError: Yup.ValidationError) => {
+            const errors: Record<string, string> = {}
+            validationError.inner.forEach(error => {
+                if(error.path){
+                    errors[error.path] = error.message
+                }
+            })
+            setFormErrors(errors)
+        })
+        
     }
+    
     return (
         <div className="results-form-page container">
     
@@ -27,14 +78,28 @@ const ResultsForm = () => {
                 <Form onSubmit={submit}>
                    <div className="form-inputs">
                        <div className="names-row">
-                           <Input type="text"  name="firstName" label="First Name"/>
-                           <Input type="text" name="lastName" label="Last Name"/>
+                           <div className="inputs">
+                               <Input type="text"  name="firstName" label="First Name" required value={values['firstName']} onChange={onInputChange}/>
+                               {formErrors.firstName && <small className="error-msg">{formErrors.firstName}</small>}
+                           </div>
+                           <div className="inputs">
+                               <Input type="text" name="lastName" label="Last Name" required value={values['lastName']} onChange={onInputChange}/>
+                               {formErrors.lastName && <small className="error-msg">{formErrors.lastName}</small>}
+                           </div>
                        </div>
-                       <Input type="tel" name="phoneNumber" label="Phone Number"/>
-                       <Input  type="email" name="email" label="Email"/>
+                       <div className="inputs">
+                           <Input type="tel" name="phoneNumber" label="Phone Number" required value={values['phoneNumber']} onChange={onInputChange}/>
+                           {formErrors.phoneNumber && <small className="error-msg">{formErrors.phoneNumber}</small>}
+                       </div>
+                       <div className="inputs">
+                           <Input  type="email" name="email" label="Email" required value={values['email']} onChange={onInputChange}/>
+                           {formErrors.email && <small className="error-msg">{formErrors.email}</small>}
+                       </div>
                    </div>
                     
-                    <div className="form-btn"><Button className="btn-primary-rounded btn-md">Get Results</Button></div>
+                    <div className="form-btn">
+                        <Button className="btn-primary-rounded btn-md" type={'submit'}>Get Results</Button>
+                    </div>
                 </Form>
                 <small className="text-dark disclaimer-results">By clicking the button above, you are providing consent to display the results of your hearing test, provide explanation about your hearing ability and present the information that will help you choose the care option best for you. Read our Privacy <a href="">Policy here</a>.</small>
             </div>
