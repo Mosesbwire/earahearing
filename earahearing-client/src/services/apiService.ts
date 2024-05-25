b3e356312ab92b782d6700484f1b606831805bda
Mosesbwire <mosesdev92@gmail.com>
2024-05-25 17:56:41 +0300

    fix

--- earahearing-client/src/services/apiService.ts
+++ earahearing-client/src/services/apiService.ts
@@ -1,6 +1,7 @@
 
 // const BASE_URL = 'http://127.0.0.1:5000'
-const BASE_URL = import.meta.env.VITE_BACKEND_API_URL
+// const BASE_URL = import.meta.env.VITE_BACKEND_API_URL
+const BASE_URL = 'https://earahearing-backend-api.onrender.com'
 
 async function sendRequest(url: string, options: RequestInit) {
     const response = await fetch(url, options)
