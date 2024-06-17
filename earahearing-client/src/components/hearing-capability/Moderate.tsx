ebaa84c94a2e0e8eae74301676f2d432aa21f97f
Mosesbwire <mosesdev92@gmail.com>
2024-06-17 11:59:56 +0300

    refactor: change text

--- earahearing-client/src/components/hearing-capability/Moderate.tsx
+++ earahearing-client/src/components/hearing-capability/Moderate.tsx
@@ -3,19 +3,19 @@ import './hearing-capability.css'
 const Moderate = () => {
     return <div>
          <p className="text text-dark cap-text">
-         Trouble hearing conversations even in quiet environments
+            Difficulty hearing conversations even in quiet environments.
          </p>
          <p className="text text-dark cap-text">
-           TV volume will be louder than you notice, you may also used closed captions
+           The TV volume may be louder than you notice; you may also use closed captions.
          </p>
          <p className="text text-dark cap-text">
-            Using the speaker phone to hear better on the phone
+            Using the speakerphone to hear better on the phone.
          </p>
          <p className="text text-dark cap-text">
-            When people are talking in a group, it is hard to hear more than one person 
+            When people are talking in a group, it is hard to hear more than one person.
          </p>
          <p className="text text-dark cap-text">
-            You may find that instead of asking for repetition, you tend to not be involved in the conversation
+            You may find that instead of asking for repetition, you tend to not be involved in the conversation.
          </p>
     </div>
 }
--- earahearing-client/src/components/hearing-capability/Severe.tsx
+++ earahearing-client/src/components/hearing-capability/Severe.tsx
@@ -3,16 +3,16 @@ import './hearing-capability.css'
 const Severe = () => {
     return <div>
          <p className="text text-dark cap-text">
-         Trouble hearing conversations in any environment
+         Trouble hearing conversations in any environment.
          </p>
          <p className="text text-dark cap-text">
-           TV volume will be very loud and you will have to have closed captions (CC) on to understand the TV
+           TV volume will be very loud and you will need closed captions (CC) to understand the TV.
          </p>
          <p className="text text-dark cap-text">
-            Using the speaker phone to hear better on the phone
+            Using the speakerphone to hear better during phone calls.
          </p>
          <p className="text text-dark cap-text">
-            Communication will be very challenging, unless the conversation is held in a quiet place with only one speaker at a time
+            Communication will be very challenging unless the conversation is held in a quiet place with only one speaker at a time.
          </p>
     </div>
 }
