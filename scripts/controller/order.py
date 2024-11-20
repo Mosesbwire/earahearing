94d68682e2a1f5a025a2766238877f0c6ebb1f32
Mosesbwire <mosesdev92@gmail.com>
2024-11-20 17:29:45 +0300

    fix

--- scripts/controller/order.py
+++ scripts/controller/order.py
@@ -1,6 +1,6 @@
 from flask import make_response, jsonify
 
-HMACKEY = c3e71ab2e7724da4a357c36793e9685da2f88fbeda68bc33d3bbbc8a89a54a6a
+# HMACKEY = c3e71ab2e7724da4a357c36793e9685da2f88fbeda68bc33d3bbbc8a89a54a6a
 def fulfilled_order(request):
     print(request)
     return make_response(jsonify({"response": "OK"}), 200)
